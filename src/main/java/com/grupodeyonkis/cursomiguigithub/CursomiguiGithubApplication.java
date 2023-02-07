12bd7f7372cde82f75c569073286357298677496
miguixinfo <miguixinfo@gmail.com>
2023-02-07 14:01:49 +0100

    Mi primer commit con Intellij

---
+++ src/main/java/com/grupodeyonkis/cursomiguigithub/CursomiguiGithubApplication.java
@@ -0,0 +1,13 @@
+package com.grupodeyonkis.cursomiguigithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomiguiGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomiguiGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupodeyonkis/cursomiguigithub/CursomiguiGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupodeyonkis.cursomiguigithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomiguiGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
