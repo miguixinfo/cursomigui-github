0d6cde35a49df21679dd9a7752210a9264ed637a
miguixinfo <miguixinfo@gmail.com>
2023-02-07 17:40:23 +0100

    Hago un controller de prueba (hasta min 00:40:00)

---
+++ src/main/java/com/grupodeyonkis/cursomiguigithub/controllers/UsuarioController.java
@@ -0,0 +1,15 @@
+package com.grupodeyonkis.cursomiguigithub.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+    @RequestMapping(value = "prueba")
+    public List<String> prueba() {
+        return List.of("manzana", "kiwi", "banana");
+    }
+
+}
