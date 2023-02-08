3b38033d9b8a3eb3a7c767b59ff37b43bb8008a6
miguixinfo <miguixinfo@gmail.com>
2023-02-08 10:22:05 +0100

    Añado la capa model con Usuario.java y UsuarioController.java en la capa controller. (hasta 1:25:55)

--- src/main/java/com/grupodeyonkis/cursomiguigithub/controllers/UsuarioController.java
+++ src/main/java/com/grupodeyonkis/cursomiguigithub/controllers/UsuarioController.java
@@ -1,15 +1,90 @@
 package com.grupodeyonkis.cursomiguigithub.controllers;
 
+import com.grupodeyonkis.migui.cursomigui.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "prueba")
-    public List<String> prueba() {
-        return List.of("manzana", "kiwi", "banana");
+
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id ) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Miguel");
+        usuario.setId(id);
+        usuario.setApellido("Gómez");
+        usuario.setEmail("gmigui@gmail.com");
+        usuario.setTelefono("635749440");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario1")
+    public Usuario editar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Miguel");
+        usuario.setApellido("Gómez");
+        usuario.setEmail("gmigui@gmail.com");
+        usuario.setTelefono("635749440");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario12")
+    public Usuario eliminar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Miguel");
+        usuario.setApellido("Gómez");
+        usuario.setEmail("gmigui@gmail.com");
+        usuario.setTelefono("635749440");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario123")
+    public Usuario buscar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Miguel");
+        usuario.setApellido("Gómez");
+        usuario.setEmail("gmigui@gmail.com");
+        usuario.setTelefono("635749440");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Miguel");
+        usuario.setId(23L);
+        usuario.setApellido("Gómez");
+        usuario.setEmail("gmigui@gmail.com");
+        usuario.setTelefono("635749440");
+
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setNombre("Marcos");
+        usuario2.setId(564L);
+        usuario2.setApellido("Polo");
+        usuario2.setEmail("marco@gmail.com");
+        usuario2.setTelefono("5432345654");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setNombre("Javi");
+        usuario3.setId(12L);
+        usuario3.setApellido("Lopez");
+        usuario3.setEmail("javi@gmail.com");
+        usuario3.setTelefono("6354321234");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        return usuarios;
     }
 
 }
