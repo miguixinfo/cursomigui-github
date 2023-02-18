b219bfc9ecdb62c34fe78e5671df786cc91e9fc4
miguixinfo <miguixinfo@gmail.com>
2023-02-18 14:20:19 +0100

    Dejamos nuestro front un poco más bonito para solamente incluir nuestras funcionaldiades. (HASTA 4:24:00 FIN DEL TUTORIAL)

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -2,7 +2,12 @@
 $(document).ready(function() {
     cargarUsuarios();
   $('#usuarios').DataTable();
+  actualizarEmailDelUsuario();
 });
+
+function actualizarEmailDelUsuario() {
+    document.getElementById('txt-email-usuario').outerHTML = localStorage.email
+}
 
 async function cargarUsuarios() {
       const request = await fetch('api/usuarios', {
